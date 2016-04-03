2747c775274f89b87a114a19e9ed32fde0b7a742
Gabriel Rodrigues <0800.grc@gmail.com>
2016-04-03 12:06:17 -0300

    Adicionando App desenvolvido

---
+++ App/CadastroVisitante/www/scripts/ajaxHelper.js
@@ -0,0 +1,28 @@
+﻿var ajaxHelper = {
+    ApiGet: function (route, sendData, callback, errorhandler) {
+        $.ajax({
+            url: "https://example-prefix-8080.codio.io/api" + route,
+            type: 'GET',
+            data: (sendData ? sendData : null)
+        }).done(function (data) {
+            if (callback)
+                callback(data);
+        }).error(function (data) {
+            if (errorhandler)
+                errorhandler(data);
+        });
+    },
+    ApiPost: function (route, sendData, callback, errorhandler) {
+        $.ajax({
+            url: "https://example-prefix-8080.codio.io/api" + route,
+            type: 'POST',
+            data: (sendData ? sendData : null)
+        }).done(function (data) {
+            if (callback)
+                callback(data);
+        }).error(function (data) {
+            if (errorhandler)
+                errorhandler(data);
+        });
+    }
+}
---
+++ App/CadastroVisitante/www/scripts/app.js
@@ -0,0 +1,33 @@
+﻿
+function GetVisitantes() {
+    $("#visitantes").html("");
+    ajaxHelper.ApiGet("/visitor", undefined, function (visitantes) {
+        $.each(visitantes, function (i, visitante) {
+            RenderVisitante(visitante);
+        });
+    });
+}
+
+function RenderVisitante(visitante) {
+    var div = document.createElement("div");
+    $(div).attr("class", "visitante");
+    $(div).attr("id", visitante._id);
+    $(div).html(visitante.name);
+
+    $("#visitantes").append(div);
+}
+function AddVisitante() {
+    var visitante = $("#nomeVisitante").val();
+    $("#nomeVisitante").val("");
+
+    ajaxHelper.ApiPost("/visitor", { name: visitante }, function () {
+        GetVisitantes();
+    });
+}
+
+$(document).ready(function () {
+    GetVisitantes();
+    $("#btnAdicionarVisitante").click(function () {
+        AddVisitante();
+    });
+});
